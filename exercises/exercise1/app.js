901be21f410692d299a1977b8de2b7890a7e1fa7
bastibhr <bastibhr@gmail.com>
2016-09-26 21:37:25 +0200

    c
    bla

--- exercises/exercise1/app.js
+++ exercises/exercise1/app.js
@@ -1,36 +1,36 @@
 (function($, window, document) {
 
-    // The $ is now locally scoped 
+	// The $ is now locally scoped
 
-   // Listen for the jQuery ready event on the document
-   $(function() {
+	// Listen for the jQuery ready event on the document
+	$(function() {
 
-     // The DOM is ready!
-	initItemList();
-	initButtons();
-   });
-   
-   
-   function initButtons(){
+		// The DOM is ready!
+		initItemList();
+		initButtons();
+	});
+
+
+	function initButtons() {
 		var deleteButton = $("#delete-button");
-		deleteButton.on("click",  function() {
+		deleteButton.on("click", function() {
 			$("#items").children().remove();
 		});
-		
+
 		var addButton = $("#add-button");
-		addButton.on("click",  function() {
+		addButton.on("click", function() {
 			var itemList = $("#items");
-			itemList.append('<li class="list-group-item">' + $("#new-item").val()+ '</li>');
+			itemList.append('<li class="list-group-item">' + $("#new-item").val() + '</li>');
 		});
 	}
-	
-	function initItemList(){
-		
+
+	function initItemList() {
+
 		$(".list-group").on("click", "li", function() {
 			$(this).toggleClass("ok");
 		});
 	}
-   
-   
 
-  }(window.jQuery, window, document));
+
+
+}(window.jQuery, window, document));
